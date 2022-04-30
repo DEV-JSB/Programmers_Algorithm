5656ddf11793535b4e3c8ee06c175b9412d26069
DEV_JSB <mvat1225@gmail.com>
2022-04-30 20:57:14 +0900

    LV1 짝수 혹은 홀수 이후 부터 C 코드는 노션에만 정리를 하자 (다른 분이 푸신 비트연산자 코드 존경..)

---
+++ AlgorythmTest/LV1_Odd_Even/Source.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    (num % 2) ? answer = "Odd" : answer = "Even";
+    return answer;
+}
+
