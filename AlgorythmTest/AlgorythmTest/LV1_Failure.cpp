0bb30987e4cd41303be0f52ee8ebd777369262ce
DEV_JSB <mvat1225@gmail.com>
2022-07-02 01:13:38 +0900

    정확성 96.3... 하.. 이런 경우는 처음인데..

--- AlgorythmTest/AlgorythmTest/LV1_Failure.cpp
+++ AlgorythmTest/AlgorythmTest/LV1_Failure.cpp
@@ -16,12 +16,10 @@ void PushAnswer(vector<int>& answer, vector<double>percent)
         for (int j = 0; j < percent.size(); ++j)
         {
             if (percent[MaxIndex] < percent[j])
-            {
                 MaxIndex = j;
-            }
         }
         percent[MaxIndex] = -1.f;
-        answer.push_back(MaxIndex+1);
+        answer.push_back(MaxIndex + 1);
     }
 }
 
@@ -40,27 +38,32 @@ vector<int> solution(int N, vector<int> stages) {
     {
         for (int j = 0; j < stages[i]; ++j)
         {
+            // 6인데 , 사실상 5의 인덱스는 접근하면 안됨
+            if (j == N)
+            {
+                StageClearCount[j-1] += 1;
+                StageTryCount[j - 1] += 1;
+                break;
+            }
             // 스테이지를 시도한 횟수를 저장한다
-            ++StageTryCount[j];
+            StageTryCount[j] += 1;
             // 스테이지를 클리어 한 횟수를 저장한다
+            // 밑의 로직은 맞지 않은 것 같다.
             if (j == stages[i] - 1)
                 continue;
-            ++StageClearCount[j];
+            StageClearCount[j] += 1;
         }
-            
     }
 
     for (int i = 0; i < N; ++i)
     {
-        std::cout << i + 1 << "번째 스테이지를 시도한 횟수 : " << StageTryCount[i] << std::endl;
-    }
-    for (int i = 0; i < N; ++i)
-    {
-        std::cout << i + 1 << "번째 스테이지를 클리어한 횟수 : " << StageClearCount[i] << std::endl;
-    }
-
-    for (int i = 0; i < N; ++i)
+        if (StageTryCount[i] == 0)
+        {
+            failurePercent.push_back(0);
+            continue;
+        }
         failurePercent.push_back(((double)StageTryCount[i] - (double)StageClearCount[i]) / (double)StageTryCount[i]);
+    }
     PushAnswer(answer, failurePercent);
 
     delete[] StageTryCount;
@@ -69,9 +72,10 @@ vector<int> solution(int N, vector<int> stages) {
     return answer;
 }
 
+//     solution(5,std::vector<int>({ 3,3,3,3 }));
 
 
 void main()
 {
-    solution(5,std::vector<int>({ 2,1,2,6,2,4,3,3 }));
+    solution(10,std::vector<int>({ 5,4,3,2 }));
 }
