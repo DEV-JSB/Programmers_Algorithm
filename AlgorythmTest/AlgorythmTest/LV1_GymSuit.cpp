8e4d43e883d8dcc21b8b7bae9586e2fd8fa38aab
DEV_JSB <mvat1225@gmail.com>
2022-07-11 02:11:07 +0900

    오늘안에 풀기

--- AlgorythmTest/AlgorythmTest/LV1_GymSuit.cpp
+++ AlgorythmTest/AlgorythmTest/LV1_GymSuit.cpp
@@ -6,15 +6,25 @@ using namespace std;
 int solution(int n, vector<int> lost, vector<int> reserve)
 {
     int answer = n;
+    bool borrow = false;
     for (int i = 0; i < lost.size(); ++i)
     {
+        borrow = false;
         for (int j = 0; j < reserve.size(); ++j)
         {
             if (lost[i] == reserve[j] - 1 || lost[i] == reserve[j] + 1)
+            {
                 reserve[j] = -1;
-            else if (lost[i] != reserve[j] - 1 || lost[i] != reserve[j] + 1)
-                --answer;
+                borrow = true;
+                break;
+            }
         }
+        if (!borrow)
+            --answer;
     }
     return answer;
+}
+void main()
+{
+    solution(5, vector<int>({ 1,2,3,4,5 }), vector<int>({ 0 }));
 }
