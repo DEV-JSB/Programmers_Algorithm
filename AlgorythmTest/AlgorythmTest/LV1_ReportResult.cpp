6f0f6e743cfad34340d1d89bcc9e44f63fb914ed
DEV_JSB <mvat1225@gmail.com>
2022-08-09 21:06:47 +0900

    100 점을 받지 못 했다 , 시간 최적화 필요..

--- AlgorythmTest/AlgorythmTest/LV1_ReportResult.cpp
+++ AlgorythmTest/AlgorythmTest/LV1_ReportResult.cpp
@@ -1,39 +1,59 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <algorithm>
 using namespace std;
 
 struct ReportInformation
 {
     int k;
     int mailCount;
+    vector<string> ReportList;
 };
 int CheckReport(vector<string>& _id, vector<string>& _report, map<string, ReportInformation>& _mapID, vector<int>& _answer)
 {
+    bool Overlap = false;;
     int BlinkIndex;
+    string ReportString;
+    string Reporter;
+    // 리포트 명단을 둘러보면서 , 리포트 한 사람의 k 번째 횟수를 차감하는 것 , 
+    // 그럼 이 떄 ReportInformation 에 내가 신고한 명단을 넣어놓고 , 내가 신고한 명단만큼만 for 문을 도는게 어떨까?
     for (size_t i = 0; i < _report.size(); ++i)
     {
         BlinkIndex = _report[i].find(" ") + 1;
-        --_mapID[_report[i].substr(BlinkIndex, _report[i].length() - BlinkIndex)].k;
+        ReportString = _report[i].substr(BlinkIndex, _report[i].length() - BlinkIndex);
+        Reporter = _report[i].substr(0, BlinkIndex - 1);
+        Overlap = false;
+
+        auto iter = find(_mapID[Reporter].ReportList.begin(), _mapID[Reporter].ReportList.end(), ReportString);
+        if (iter != _mapID[Reporter].ReportList.end())
+            Overlap = true;
+        if (!Overlap)
+        {
+            --_mapID[_report[i].substr(BlinkIndex, _report[i].length() - BlinkIndex)].k;
+            _mapID[Reporter].ReportList.push_back(ReportString);
+        }
     }
     for (size_t i = 0; i < _id.size(); ++i)
     {
         if (0 >= (*_mapID.find(_id[i])).second.k)
         {
-            for (size_t j = 0; j < _report.size(); ++j)
+            for (size_t k = 0; k < _id.size(); ++k)
             {
-                BlinkIndex = _report[j].find(" ")+1;
-                if (_id[i] == _report[j].substr(BlinkIndex, _report[j].length() - BlinkIndex))
-                    _mapID[_report[j].substr(0, BlinkIndex-1)].mailCount++;
+                if (_id[i] == _id[k])
+                    continue;
+                for (size_t j = 0; j < _mapID[_id[k]].ReportList.size(); ++j)
+                {
+                    if (_mapID[_id[k]].ReportList[j] == _id[i])
+                        _mapID[_id[k]].mailCount++;
+                }
             }
         }
     }
-
     for (size_t i = 0; i < _id.size(); ++i)
     {
         _answer.push_back(_mapID[_id[i]].mailCount);
     }
-
     return 0;
 }
 
@@ -49,27 +69,6 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k) {
         mapID.insert({ id_list[i],tmp });
     }
 
-    // 한 유저를 여러번 신고할 경우의 필터링
-    for (auto iter = report.begin(); iter != report.end(); ++iter)
-    {
-        for (auto iter2 = iter + 1; iter2 != report.end();)
-        {
-            if ((*iter) == (*iter2))
-                iter2 = report.erase(iter2);
-            else
-                ++iter2;
-        }
-    }
-    // 이제 같은 놈이 같은 애를 신고할 일은 없어졌다 . 
-    // 그렇게 되면 앞의 문자는 쓸모 없게 되는 것이 아닐까?
-
-    // 신고를 한 모든 유저에게 발송을 하네...
-
     CheckReport(id_list, report, mapID, answer);
     return answer;
-}
-
-void main()
-{
-    solution(vector<string>({ "muzi", "frodo", "apeach", "neo" }), vector<string>({ "muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi" }), 2);
 }
