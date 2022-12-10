0c7eba6d16434529fc287bac0dbcc4086e7044e6
DEV_JSB <mvat1225@gmail.com>
2022-12-10 23:37:54 +0900

    20221209 알고리즘 다시 풀기 시작

--- AlgorythmTest/AlgorythmTest/LV0_CutPaper.cpp
+++ AlgorythmTest/AlgorythmTest/LV0_CutPaper.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
-int solution(int M, int N) {
-    return M * N - 1;
+int solution(vector<int> common) {
+    // 0번째와 1번째 그리고 1번째와 2번째의 차가 등차 인지 등비인지 구별한다.
+    int BetweenCorrection1 = common[1] - common[0];
+    int BetweenCorrection2 = common[2] - common[1];
+
+    return BetweenCorrection1 == BetweenCorrection2 
+        ? common[common.size() - 1] + BetweenCorrection1 : common[common.size() - 1] * common[1] / common[0];
 }
