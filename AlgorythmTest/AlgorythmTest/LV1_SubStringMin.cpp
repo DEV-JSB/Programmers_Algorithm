7b53b42eca8e2436848b25b126f80d8df7625acd
DEV_JSB <mvat1225@gmail.com>
2023-01-03 22:15:51 +0900

    레벨1크기가작은부분문자열 푸는중

---
+++ AlgorythmTest/AlgorythmTest/LV1_SubStringMin.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string t, string p) {
+    vector<int> t_Nums;
+    
+    // p 가 몇 자리 수 인지 분석한다.
+    int pCount = p.length();
+    
+    // p 의 자릿 수 만큼 t 를 나눈다.
+    for (int i = 0; i < t.size(); ++i)
+    {
+        
+    }
+
+
+    int answer = 0;
+    return answer;
+}
