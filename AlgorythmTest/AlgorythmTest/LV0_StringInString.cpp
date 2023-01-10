e0eab6c4fd624eacba8c06b980f8ceccb7ec92df
DEV_JSB <mvat1225@gmail.com>
2023-01-11 02:13:30 +0900

    __

---
+++ AlgorythmTest/AlgorythmTest/LV0_StringInString.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+
+int solution(string str1, string str2) {
+    int answer = 0;
+
+    for (int i = 0; i < str1.length(); ++i)
+    {
+        if (str1[i] == str2[i])
+        {
+            for (int j = 0; j < str2.length(); ++j)
+            {
+            }
+        }
+    }
+
+    return answer;
+}
