775e5d4ada3ef8ac943d8c595d406981eeaad49c
DEV_JSB <mvat1225@gmail.com>
2023-01-12 21:28:06 +0900

    LV0_문자열안에문자열

--- AlgorythmTest/AlgorythmTest/LV0_StringInString.cpp
+++ AlgorythmTest/AlgorythmTest/LV0_StringInString.cpp
@@ -10,13 +10,17 @@ int solution(string str1, string str2) {
 
     for (int i = 0; i < str1.length(); ++i)
     {
-        if (str1[i] == str2[i])
+        if (str1[i] == str2[0])
         {
             for (int j = 0; j < str2.length(); ++j)
             {
+                if (j + 1 == str2.length() && str1[i + j] == str2[j])
+                    return 1;
+                else if (str1[i + j] != str2[j])
+                    break;
             }
         }
     }
 
-    return answer;
+    return 2;
 }
