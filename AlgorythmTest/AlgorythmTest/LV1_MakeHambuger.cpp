00891955a06d095f77c87159c9cd69856d9cce09
DEV_JSB <mvat1225@gmail.com>
2023-01-12 22:27:49 +0900

    레벨1 햄버거만들기 66.7

---
+++ AlgorythmTest/AlgorythmTest/LV1_MakeHambuger.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> ingredient) {
+    int answer = 0;
+    int countIndex = 0;
+
+    while (countIndex <= ingredient.size() - 4)
+    {
+        for (vector<int>::iterator iter = ingredient.begin();  countIndex <= ingredient.size() - 4; ++iter, ++countIndex)
+        {
+            if (ingredient.size() < 4)
+                return answer;
+            if ((*iter) == 1 && *(iter + 1) == 2 && *(iter + 2) == 3 && *(iter + 3) == 1)
+            {
+                for (int i = 0; i < 4; ++i)
+                    iter = ingredient.erase(iter);
+                ++answer;
+                iter = ingredient.begin();
+                countIndex = 0;
+            }
+        }
+    }
+
+    return answer;
+}
+
+void main()
+{
+    solution({ 2, 1, 1, 2, 3, 1, 2, 3, 1 });
+}
