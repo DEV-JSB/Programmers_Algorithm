009b3d07daba69d1642541672c1b8d8f14d77ee2
YoonChan <103971614+DEV-JSB@users.noreply.github.com>
2023-01-14 16:26:41 +0900

    레벨1 햄버거 만들기 계속 푸는중 시간 개선 필요

--- AlgorythmTest/AlgorythmTest/LV1_MakeHambuger.cpp
+++ AlgorythmTest/AlgorythmTest/LV1_MakeHambuger.cpp
@@ -5,25 +5,24 @@ using namespace std;
 
 int solution(vector<int> ingredient) {
     int answer = 0;
-    int countIndex = 0;
-
-        for (vector<int>::iterator iter = ingredient.begin();  countIndex <= ingredient.size() - 4; ++iter, ++countIndex)
+    int countIdx = 0;
+    for (vector<int>::iterator iter = ingredient.begin(); countIdx <= ingredient.size() - 4;)
+    {
+        if (ingredient.size() < 4)
+            return answer;
+        if ((*iter) == 1 && *(iter + 1) == 2 && *(iter + 2) == 3 && *(iter + 3) == 1)
         {
-            if (ingredient.size() < 4)
-                return answer;
-            if ((*iter) == 1 && *(iter + 1) == 2 && *(iter + 2) == 3 && *(iter + 3) == 1)
-            {
-                for (int i = 0; i < 4; ++i)
-                    iter = ingredient.erase(iter);
-                ++answer;
-                iter = ingredient.begin();
-                countIndex = 0;
-            }
+            for (int i = 0; i < 4; ++i)
+                iter = ingredient.erase(iter);
+            ++answer;
+            countIdx = 0;
+            iter = ingredient.begin();
         }
+        else
+        {
+            ++countIdx;
+            ++iter;
+        }
+    }
     return answer;
-}
-
-void main()
-{
-    solution({ 2, 1, 1, 2, 3, 1, 2, 3, 1 });
 }
