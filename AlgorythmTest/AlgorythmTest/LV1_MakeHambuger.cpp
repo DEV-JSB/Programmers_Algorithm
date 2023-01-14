89ec96b6211c641168cb95541ddd55d62547a107
YoonChan <103971614+DEV-JSB@users.noreply.github.com>
2023-01-14 16:58:17 +0900

    LV1_햄버거 만들기 풀었다

--- AlgorythmTest/AlgorythmTest/LV1_MakeHambuger.cpp
+++ AlgorythmTest/AlgorythmTest/LV1_MakeHambuger.cpp
@@ -5,24 +5,24 @@ using namespace std;
 
 int solution(vector<int> ingredient) {
     int answer = 0;
-    int countIdx = 0;
-    for (vector<int>::iterator iter = ingredient.begin(); countIdx <= ingredient.size() - 4;)
+    for (vector<int>::iterator iter = ingredient.begin() + 3 ; iter != ingredient.end();)
     {
         if (ingredient.size() < 4)
             return answer;
-        if ((*iter) == 1 && *(iter + 1) == 2 && *(iter + 2) == 3 && *(iter + 3) == 1)
+        if ((*iter) == 1 && *(iter - 1) == 3 && *(iter - 2) == 2 && *(iter - 3) == 1)
         {
+            iter = iter - 3;
             for (int i = 0; i < 4; ++i)
                 iter = ingredient.erase(iter);
             ++answer;
-            countIdx = 0;
-            iter = ingredient.begin();
         }
         else
-        {
-            ++countIdx;
             ++iter;
-        }
     }
     return answer;
+}
+
+void main()
+{
+    solution({ 1,2,3,4,5,6 });
 }
