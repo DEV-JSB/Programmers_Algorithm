6f4cc1b6ad2af23f2f22ab00d72cb46ffaea2eef
YoonChan <103971614+DEV-JSB@users.noreply.github.com>
2023-01-25 16:53:41 +0900

    레벨0 배열 원소 길이

---
+++ AlgorythmTest/AlgorythmTest/LV0_ArrayElementLength.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> strlist) {
+    vector<int> answer;
+    for (string element : strlist)
+        answer.push_back(element.length());
+    return answer;
+}
