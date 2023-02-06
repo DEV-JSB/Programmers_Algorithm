3edd7c3ed994a841dbccc27594bb79cd70537083
DEV_JSB <mvat1225@gmail.com>
2023-02-06 16:39:58 +0900

    레벨0 구슬을 나누는 경우의 수

--- AlgorythmTest/AlgorythmTest/LV0_DivideMarble.cpp
+++ AlgorythmTest/AlgorythmTest/LV0_DivideMarble.cpp
@@ -12,14 +12,30 @@ unsigned long long Factorial(int excaption,int end)
     return sum;
 }
 
+unsigned long long Factorial2(int excaption, int end , unsigned long long denominator)
+{
+    bool Divide = false;
+    unsigned long long sum = excaption + 1;
+    for (int i = sum + 1; i <= end; ++i)
+    {
+        sum *= i;
+        if (Divide == false && sum % denominator == 0)
+        {
+            sum /= denominator;
+            Divide = true;
+        }
+    }
+    return sum;
+}
+
 unsigned long long solution(int balls, int share) {
     if (balls == share)
         return 1;
     unsigned long long answer;
     if(balls - share > share)
-        answer = (Factorial(balls - share , balls) / Factorial(0,share));
+        answer = Factorial2(balls - share , balls, Factorial(0, share));
     else
-        answer = Factorial(share, balls) / Factorial(0, balls - share);
+        answer = Factorial2(share, balls, Factorial(0, balls - share));
     return answer;
 }
 void main()
