1037d4dfa7e7ad6af92a55c590427c48813b776f
DEV_JSB <mvat1225@gmail.com>
2023-02-10 21:52:49 +0900

    레벨0 문자열 정렬하기(1)

--- AlgorythmTest/AlgorythmTest/LV0_StringSort1.cpp
+++ AlgorythmTest/AlgorythmTest/LV0_StringSort1.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+
+
 vector<int> solution(string my_string) {
     vector<int> answer;
     for(int i = 0 ; i < my_string.length() ; ++i)
@@ -10,5 +12,20 @@ vector<int> solution(string my_string) {
         if (my_string[i] >= '0' && my_string[i] <= '9')
             answer.push_back(my_string[i] - '0');
     }
+    for (int i = 0; i < answer.size() - 1; ++i)
+    {
+        for (int j = 0; j < answer.size() - (i + 1); ++j)
+        {
+            if (answer[j] > answer[j + 1])
+            {
+                int tmp = answer[j];
+                answer[j] = answer[j + 1];
+                answer[j + 1] = tmp;
+            }
+        }
+    }
+
+
+
     return answer;
 }
