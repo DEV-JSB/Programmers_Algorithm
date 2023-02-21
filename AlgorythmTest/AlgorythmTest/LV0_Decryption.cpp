43064e81b6182aa3fefee3f6ff2cbe60827aae86
DEV_JSB <mvat1225@gmail.com>
2023-02-22 08:56:37 +0900

    레벨0 암호 해독

--- AlgorythmTest/AlgorythmTest/LV0_Decryption.cpp
+++ AlgorythmTest/AlgorythmTest/LV0_Decryption.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 string solution(string cipher, int code) {
     string answer = "";
-
+    int multiple = code;
+    for (int i = code; i <= cipher.length(); i += multiple)
+    {
+        answer += cipher[i-1];
+    }
     
 
     return answer;
