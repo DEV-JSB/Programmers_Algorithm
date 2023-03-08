87c70ec235e6d4ba941a336c3f6199c1a1a63b80
YoonChan <103971614+DEV-JSB@users.noreply.github.com>
2023-03-08 14:20:28 +0900

    레벨2 숫자의 표현

---
+++ AlgorythmTest/AlgorythmTest/LV2_NumberExpression.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) 
+{
+    int answer{ 0 };
+    int sum{ 0 };
+    for (int i{ 1 }; i <= n; ++i)
+    {
+        sum = 0;
+        for (int j{ i }; sum <= n; ++j)
+        {
+            sum += j;
+            if (sum == n)
+            {
+                ++answer;
+                break;
+            }
+        }
+    }
+    return answer;
+}
+void main()
+{
+    solution(1);
+}
