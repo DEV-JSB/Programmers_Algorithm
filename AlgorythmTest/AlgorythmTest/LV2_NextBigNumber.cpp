85fec0afb39899dc2c5c3e64b54e59b83ead9a44
DEV_JSB <mvat1225@gmail.com>
2023-03-11 19:08:41 +0900

    레벨2 다음 큰 숫자_문해력 심각함,재시도 요함

--- AlgorythmTest/AlgorythmTest/LV2_NextBigNumber.cpp
+++ AlgorythmTest/AlgorythmTest/LV2_NextBigNumber.cpp
@@ -1,46 +1,73 @@
 #include <string>
 #include <vector>
+#include <math.h>
 
+#include <iostream>
 using namespace std;
 
-int solution(int n) {
-    int answer{ 0 };
-    string binaryStr{ "" };
-    bool translateSuccess{ false };
-    while (n)
+void GetCombination(int bit, const int ZeroCount, const int OneCount, int one, int zero, string str, const int n, int* MinNum)
+{
+    str.push_back('0' + bit);
+    if (bit)
+        ++one;
+    else
+        ++zero;
+    if (0 != str.size() && str[0] == '0')
+        return;
+    else if (((int)str.length() == ZeroCount + OneCount))
     {
-        binaryStr += '0' + n % 2;
-        n /= 2;
-    }
-    for (int i{ 0 }; i < binaryStr.size(); ++i)
-    {
-        if ('0' == binaryStr[i])
+        if (ZeroCount == zero && OneCount == one)
         {
-            translateSuccess = true;
-            break;
-        }
-    }
-    if (translateSuccess)
-    {
-        for (int i{ 0 }, num{ 1 }; i < binaryStr.size(); ++i, num *= 2)
-        {
-            if ('1' == binaryStr[i] && n - num > n)
+            int num{ 1 };
+            int tmp{ 0 };
+            for (int i{ (int)str.length() - 1 }; 0 <= i; --i)
             {
-                return n - num + 1;
+                tmp += num * (str[i] - '0');
+                num *= 2;
             }
-            answer += num * (binaryStr[i] == '1' ? 1 : 0);
+            if (n < tmp && tmp < *MinNum)
+                *MinNum = tmp;
+            return;
         }
+        else
+            return;
+    }
+
+    GetCombination(1, ZeroCount, OneCount, one, zero, str, n, MinNum);
+    GetCombination(0, ZeroCount, OneCount, one, zero, str, n, MinNum);
+
+}
+
+int solution(int n) {
+    if (n == 1)
+        return 2;
+    int answer{ 1000000};
+    int tmp{ n };
+    string binaryStr{ "" };
+    int ZeroCount{ 0 };
+    int OneCount{ 0 };
+    while (tmp)
+    {
+        tmp % 2 == 0 ? ++ZeroCount : ++OneCount;
+        tmp /= 2;
+    }
+    if(0 == ZeroCount)
+    {
+        answer = n << 1;
+        answer ^= (1 << OneCount - 1);
+        ++answer;
     }
     else
     {
-        ++answer;
-        for (int i{ 0 }, num{ 2 }; i < binaryStr.size(); ++i, num *= 2)
-        {
-            if (i == binaryStr.size() - 2)
-                continue;
-            answer += num * (binaryStr[i] == '1' ? 1 : 0);
-        }
+        int RealtimeZero{ 0 };
+        int RealtimeOne{ 0 };
+        GetCombination(1 , ZeroCount, OneCount, RealtimeOne, RealtimeZero, binaryStr, n, &answer);
     }
     
     return answer;
+}
+
+void main()
+{
+    solution(4);
 }
