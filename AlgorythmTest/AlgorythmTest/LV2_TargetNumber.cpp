a551b6ddc91131ab2e8d666d0e2858278bdbaeb9
DEV_JSB <mvat1225@gmail.com>
2023-03-14 00:27:43 +0900

    레벨2 타겟 넘버 효율성 개선 코드

--- AlgorythmTest/AlgorythmTest/LV2_TargetNumber.cpp
+++ AlgorythmTest/AlgorythmTest/LV2_TargetNumber.cpp
@@ -2,28 +2,20 @@
 #include <vector>
 using namespace std;
 
-void DFS(bool isPlus,int& answer,vector<int>& numbers, int realtimeCount,const int target)
+void DFS(int numberSum ,int& answer,vector<int>& numbers, int realtimeCount,const int target)
 {
-    if ((isPlus && numbers[realtimeCount] < 0) || (!isPlus && numbers[realtimeCount] > 0))
-        numbers[realtimeCount] *= -1;
-
-    if (realtimeCount == numbers.size() - 1)
-    {
-        
-        int tmp{ 0 };
-        for (int i : numbers)
-            tmp += i;
-        if(tmp == target)
-            ++answer;
-        return;
-    }
-    DFS(true, answer, numbers, realtimeCount + 1, target);
-    DFS(false, answer, numbers, realtimeCount + 1, target);
+        if (realtimeCount == numbers.size())
+        {
+            if (numberSum == target)
+                ++answer;
+            return;
+        }
+    DFS(numberSum + numbers[realtimeCount], answer, numbers, realtimeCount + 1, target);
+    DFS(numberSum - numbers[realtimeCount], answer, numbers, realtimeCount + 1, target);
 }
-
 int solution(vector<int> numbers, int target) {
     int answer = 0;
-    DFS(true, answer, numbers, 0, target);
-    DFS(false, answer, numbers, 0, target);
+    DFS(0, answer, numbers, 0, target);
+
     return answer;
 }
