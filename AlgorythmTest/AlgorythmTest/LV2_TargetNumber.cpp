1d423df4ffe75fd520c6177d88ace55b5acda629
DEV_JSB <mvat1225@gmail.com>
2023-03-13 22:42:22 +0900

    레벨2 타겟 넘버 DFS 성공!!! 예스!

--- AlgorythmTest/AlgorythmTest/LV2_TargetNumber.cpp
+++ AlgorythmTest/AlgorythmTest/LV2_TargetNumber.cpp
@@ -1,10 +1,29 @@
 #include <string>
 #include <vector>
-
 using namespace std;
+
+void DFS(bool isPlus,int& answer,vector<int>& numbers, int realtimeCount,const int target)
+{
+    if ((isPlus && numbers[realtimeCount] < 0) || (!isPlus && numbers[realtimeCount] > 0))
+        numbers[realtimeCount] *= -1;
+
+    if (realtimeCount == numbers.size() - 1)
+    {
+        
+        int tmp{ 0 };
+        for (int i : numbers)
+            tmp += i;
+        if(tmp == target)
+            ++answer;
+        return;
+    }
+    DFS(true, answer, numbers, realtimeCount + 1, target);
+    DFS(false, answer, numbers, realtimeCount + 1, target);
+}
 
 int solution(vector<int> numbers, int target) {
     int answer = 0;
-    int bitOpereatorFlag{ 0 };
+    DFS(true, answer, numbers, 0, target);
+    DFS(false, answer, numbers, 0, target);
     return answer;
 }
