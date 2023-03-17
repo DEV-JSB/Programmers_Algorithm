4cef62230d81c652896e6d50ac53087977795cf7
DEV_JSB <mvat1225@gmail.com>
2023-03-17 13:26:52 +0900

    레벨2 멀리 뛰기 재귀 올바른버전

--- AlgorythmTest/AlgorythmTest/LV2_LongJump.cpp
+++ AlgorythmTest/AlgorythmTest/LV2_LongJump.cpp
@@ -9,7 +9,7 @@ int FiboNachi(int count, int num, int answer, int prevNum)
     answer %= 1234567;
     if (count == num)
         return answer;
-    FiboNachi(count + 1, num, answer, answer - prevNum);
+    return FiboNachi(count + 1, num, answer, answer - prevNum);
 }
 int solution(int n) {
     return FiboNachi(0, n, 0, 1);
