76a2af3a61045b616a177b583a71dc1795d85bd5
DEV_JSB <mvat1225@gmail.com>
2023-03-22 21:16:11 +0900

    레벨2 짝지어 제거하기 아...난 바보다...

--- AlgorythmTest/AlgorythmTest/LV2_StringMateDelete.cpp
+++ AlgorythmTest/AlgorythmTest/LV2_StringMateDelete.cpp
@@ -1,55 +1,22 @@
-#include <iostream>
 #include<string>
+#include<stack>
 using namespace std;
 
 int solution(string s)
 {
-    int charCount[26]{};
-    char prevChar{ ' ' };
-    string str{ s };
-    string newStr{ "" };
-    bool deletedString{ false };
-    while (true)
+    stack<char> strStack;
+    for (size_t i{ 0 }; i < s.length(); ++i)
     {
-        deletedString = false;
-        for (size_t i{ 0 }; i < str.length(); ++i)
-        {
-            if (str[i] == str[i + 1])
-            {
-                ++i;
-                deletedString = true;
-                continue;
-            }
-            else
-            {
-                newStr += str[i];
-                ++charCount[s[i] - 'a'];
-            }
-        }
-        for (int i{ 0 }; i < 26; ++i)
-        {
-            if (charCount[i] % 2 != 0)
-                return 0;
-            else
-                charCount[i] = 0;
-        }
-        if (deletedString)
-        {
-            if (0 == newStr.size())
-                return 1;
-            else
-            {
-                str = newStr;
-                newStr = "";
-            }
-        }
+        if (strStack.empty())
+            strStack.push(s[i]);
+        else if (strStack.top() == s[i])
+            strStack.pop();
         else
-            return 0;
+            strStack.push(s[i]);
     }
-    return 0;
-}
+    if (strStack.size() == 0)
+        return 1;
+    else
+        return 0;
 
-void main()
-{
-    solution("ppoooooopplaallllllaaaallalaall");
 }
