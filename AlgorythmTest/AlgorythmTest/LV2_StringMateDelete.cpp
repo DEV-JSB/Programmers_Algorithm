e3447bbe6f96d1cc7f2a162c0dc01580cadc0c51
DEV-JSB <103971614+DEV-JSB@users.noreply.github.com>
2023-03-22 15:02:27 +0900

    레벨2 짝지어 제거하기 아아.. 반례가 뭐가있지...??

--- AlgorythmTest/AlgorythmTest/LV2_StringMateDelete.cpp
+++ AlgorythmTest/AlgorythmTest/LV2_StringMateDelete.cpp
@@ -2,17 +2,16 @@
 #include<string>
 using namespace std;
 
-
-// baabaa
-
 int solution(string s)
 {
+    int charCount[26]{};
     char prevChar{ ' ' };
     string str{ s };
     string newStr{ "" };
     bool deletedString{ false };
     while (true)
     {
+        deletedString = false;
         for (size_t i{ 0 }; i < str.length(); ++i)
         {
             if (str[i] == str[i + 1])
@@ -22,7 +21,17 @@ int solution(string s)
                 continue;
             }
             else
+            {
                 newStr += str[i];
+                ++charCount[s[i] - 'a'];
+            }
+        }
+        for (int i{ 0 }; i < 26; ++i)
+        {
+            if (charCount[i] % 2 != 0)
+                return 0;
+            else
+                charCount[i] = 0;
         }
         if (deletedString)
         {
@@ -37,6 +46,10 @@ int solution(string s)
         else
             return 0;
     }
-    
     return 0;
+}
+
+void main()
+{
+    solution("ppoooooopplaallllllaaaallalaall");
 }
