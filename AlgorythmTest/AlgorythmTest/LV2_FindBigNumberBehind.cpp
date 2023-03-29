39fe7f93fb91b40a6544452d1b02d5f98ebdb4cd
YoonChan <103971614+DEV-JSB@users.noreply.github.com>
2023-03-29 15:56:22 +0900

    레벨2 뒤에 있는 큰 수 찾기 중복 처리 완료 하지만 시간 초과

--- AlgorythmTest/AlgorythmTest/LV2_FindBigNumberBehind.cpp
+++ AlgorythmTest/AlgorythmTest/LV2_FindBigNumberBehind.cpp
@@ -1,19 +1,40 @@
 #include <string>
 #include <vector>
 using namespace std;
-vector<int> solution(vector<int> numbers) 
+vector<int> solution(vector<int> numbers)
 {
     vector<int> answer;
     bool findBigNum{ false };
+    int duplicationCount{ 0 };
     for (size_t i{ 0 }; i < numbers.size() - 1; ++i)
     {
         findBigNum = false;
+        duplicationCount = 0;
+        while (numbers[i] == numbers[i + 1])
+        {
+            if (i + 1 == numbers.size() - 1)
+            {
+                for (int j{ 0 }; j < duplicationCount + 1; ++j)
+                    answer.push_back(-1);
+                i = numbers.size();
+                findBigNum = true;
+                break;
+            }
+            ++duplicationCount;
+            ++i;
+        }
         for (size_t j{ i + 1 }; j < numbers.size(); ++j)
         {
             if (numbers[i] < numbers[j])
             {
                 findBigNum = true;
-                answer.push_back(numbers[j]);
+                if (duplicationCount != 0)
+                {
+                    for (int i{ 0 }; i <= duplicationCount; ++i)
+                        answer.push_back(numbers[j]);
+                }
+                else
+                    answer.push_back(numbers[j]);
                 break;
             }
         }
@@ -22,8 +43,4 @@ vector<int> solution(vector<int> numbers)
     }
     answer.push_back(-1);
     return answer;
-}
-void main()
-{
-    solution({ 2,3,3,5 });
 }
