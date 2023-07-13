91d94a054b37fda594e15d9f017d1f36cfe6e7ca
sujo <mvat1225@naver.com>
2023-07-14 00:01:03 +0900

    String Replace 함수를 사용한 레벨0 문자열 겹쳐쓰기

--- AlgorythmTest/AlgorythmTest/LV0_OverwriteString.cpp
+++ AlgorythmTest/AlgorythmTest/LV0_OverwriteString.cpp
@@ -5,13 +5,6 @@ using namespace std;
 
 string solution(string my_string, string overwrite_string, int s) {
     string answer = "";
-    int index = s;
-
-    for (char c : overwrite_string)
-    {
-        my_string[index] = c;
-        ++index;
-    }
-    answer = my_string;
+    answer = my_string.replace(s, overwrite_string.size(), overwrite_string);
     return answer;
 }
