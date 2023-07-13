9593c7a9f177384113f2f6c04b9781ff73536cb7
sujo <mvat1225@naver.com>
2023-07-13 16:21:14 +0900

    레벨0 특수문자 출력하기

--- AlgorythmTest/AlgorythmTest/LV0_StringFlip2.cpp
+++ AlgorythmTest/AlgorythmTest/LV0_StringFlip2.cpp
@@ -8,13 +8,9 @@ string solution(string my_string, int s, int e) {
     answer = my_string;
     int translateCount;
     translateCount = (e - s) / 2;
-    for (int i = 0,start = s , end = e;
-        i <= translateCount
-        ; ++i , ++start,--end)
+    for (int i = 0 ; i <= translateCount ; ++i)
     {
-        char tmp = answer[end];
-        answer[end] = answer[start];
-        answer[start] = tmp;
+        swap(answer[e - i], answer[s + i]);
     }
 
     return answer;
