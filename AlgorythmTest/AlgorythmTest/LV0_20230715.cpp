bf50f1f5f7a00a0e4fada8931171c82076bfd8a4
sujo <mvat1225@naver.com>
2023-07-15 03:54:21 +0900

    레벨0 마지막 두 원소

--- AlgorythmTest/AlgorythmTest/LV0_20230715.cpp
+++ AlgorythmTest/AlgorythmTest/LV0_20230715.cpp
@@ -67,7 +67,7 @@ int ProductAndSumOfElements(vector<int> num_list) {
     return plusValue < multipleValue ? 0 : 1;
 }
 
-int solution(vector<int> num_list) {
+int StickOddEvenNum(vector<int> num_list) {
     int answer = 0;
     int oddSum = 0;
     int evenSum = 0;
@@ -88,4 +88,12 @@ int solution(vector<int> num_list) {
     evenSum /= 10;
     answer = oddSum + evenSum;
     return answer;
+}
+
+vector<int> LastTwoElements(vector<int> num_list) {
+    int lastNum = num_list[num_list.size() - 1];
+    int lastBackNum = num_list[num_list.size() - 2];
+    
+    num_list.push_back(lastBackNum < lastNum ? lastNum - lastBackNum : lastNum * 2);
+    return num_list;
 }
