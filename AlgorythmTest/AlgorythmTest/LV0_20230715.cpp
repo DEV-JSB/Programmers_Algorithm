47ca3c01fa2e38a3cd9dd17e7dd5c251ed3d4f99
sujo <mvat1225@naver.com>
2023-07-15 03:13:38 +0900

    레벨0 등차수열의 특정한 항만 더하기

--- AlgorythmTest/AlgorythmTest/LV0_20230715.cpp
+++ AlgorythmTest/AlgorythmTest/LV0_20230715.cpp
@@ -25,3 +25,15 @@ vector<int> AddElementsArray(vector<int> arr) {
 
     return answer;
 }
+
+int AddSpecificSum(int a, int d, vector<bool> included) {
+    int answer = 0;
+    int num = a;
+    for (int i = 0; i < included.size(); ++i)
+    {
+        if (included[i])
+            answer += num;
+        num += d;
+    }
+    return answer;
+}
