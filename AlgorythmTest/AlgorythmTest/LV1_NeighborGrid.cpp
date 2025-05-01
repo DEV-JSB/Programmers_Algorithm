0bab48c4333cbdb077b72a5df5dd02afd746554e
조수빈 <103971614+DEV-JSB@users.noreply.github.com>
2025-05-01 23:03:46 +0900

    레벨1 이웃한 칸 코드개선 (feat.chatgpt)
    GPT 조언에 따른 코드 개선

--- AlgorythmTest/AlgorythmTest/LV1_NeighborGrid.cpp
+++ AlgorythmTest/AlgorythmTest/LV1_NeighborGrid.cpp
@@ -20,3 +20,37 @@ int solution(vector<vector<string>> board, int h, int w)
 
     return answer;
 }
+
+int solution2(vector<vector<string>> board, int h, int w)
+{
+    int answer = 0;
+    int width = board[0].size();
+    int height = board.size();
+
+    int direction[4][2]{ {1,0},{-1,0},{0,1},{0,-1} };
+
+    string word = board[h][w];
+    for (int i = 0; i < 4; ++i)
+    {
+        int checkPosX = w + direction[i][1];
+        int checkPosY = h + direction[i][0];
+        if (checkPosX < 0 || checkPosY < 0 || checkPosX >= width || checkPosY >= height)
+        {
+            continue;
+        }
+        answer += word == board[checkPosY][checkPosX];
+    }
+
+    return answer;
+
+}
+
+void main()
+{
+    solution2({
+        {"blue", "red", "orange", "red"},
+        {"red", "red", "blue", "orange"},
+        {"blue", "orange", "red", "red"},
+        {"orange", "orange", "red", "blue"}
+        }, 1, 1);
+}
