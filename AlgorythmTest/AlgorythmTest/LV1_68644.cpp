c52faa6e0a8caff60bfaaa151d57e2bf4b5b3b82
조수빈 <103971614+DEV-JSB@users.noreply.github.com>
2025-07-25 16:33:21 +0900

    레벨1 두 개 뽑아서 더하기 개선
    중복 원소를 알아서 제거해주기 위해서 자료구조 set 을 사용하고 for 문을 개선했다

--- AlgorythmTest/AlgorythmTest/LV1_68644.cpp
+++ AlgorythmTest/AlgorythmTest/LV1_68644.cpp
@@ -1,27 +1,24 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <set>
+
 using namespace std;
 
 vector<int> solution(vector<int> numbers) 
 {
-    vector<int> answer;
+    set<int> set;
 
     for (int i = 0; i < numbers.size(); ++i)
     {
-        for (int j = 0; j < numbers.size(); ++j)
+        for (int j = i + 1; j < numbers.size(); ++j)
         {
-            if (i == j)
-            {
-                continue;
-            }
-            answer.push_back(numbers[i] + numbers[j]);
+            set.insert(numbers[i] + numbers[j]);
         }
     }
 
-    sort(answer.begin(), answer.end());
-    answer.erase(unique(answer.begin(), answer.end()), answer.end());
 
+    vector<int> answer{set.begin(),set.end()};
 
     return answer;
 }
