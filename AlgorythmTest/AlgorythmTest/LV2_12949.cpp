56912c40614a04719bfd055713a769f689c8d4c4
조수빈 <103971614+DEV-JSB@users.noreply.github.com>
2025-07-26 15:05:47 +0900

    레벨2 행렬의 곱셈 개선
    assign 미리 크기를 할당을 해서 개선한 코드

--- AlgorythmTest/AlgorythmTest/LV2_12949.cpp
+++ AlgorythmTest/AlgorythmTest/LV2_12949.cpp
@@ -7,22 +7,18 @@ vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
 {
     vector<vector<int>> answer;
 
+    answer.assign(arr1.size(), vector<int>(arr2[0].size(), 0));
+
     for (int i = 0; i < arr1.size(); ++i)
     {
-        answer.push_back(vector<int>{});
         for (int j = 0; j < arr2[0].size(); ++j)
         {
-            int elementValue = 0;
             for (int k = 0; k < arr1[i].size(); ++k)
             {
-                elementValue += arr1[i][k] * arr2[k][j];
+                answer[i][j] += arr1[i][k] * arr2[k][j];
             }
-            answer[i].push_back(elementValue);
         }
     }
-
-    
-
     return answer;
 }
 
