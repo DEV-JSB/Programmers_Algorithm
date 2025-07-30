400aa6ac1f42864867e813ee426d64a50127c711
Hambugi <mvat1225@naver.com>
2025-07-30 21:49:41 +0900

    레벨2 괄호 회전하기 수정
    answer 을 증감시키는 부분을 조금 개선했다

--- AlgorythmTest/AlgorythmTest/LV2_76502.cpp
+++ AlgorythmTest/AlgorythmTest/LV2_76502.cpp
@@ -43,19 +43,30 @@ int solution(string s)
     int answer = 0;
     for (int i = 0; i < s.length(); ++i)
     {
+        int checkCount = 0;
         stack<char> charStack;
         for (int j = i; j < i + s.length(); ++j)
         {
-            if (!CheckValidPair(charStack, s[j % (s.length())]))
+            if (CheckValidPair(charStack, s[j % (s.length())]))
+            {
+                ++checkCount;
+            }
+            else
             {
                 break;
             }
-            if (charStack.empty() && j + 1 == i + s.length())
-            {
-                ++answer;
-            }
         }
+        if (charStack.empty() && checkCount == s.length())
+        {
+            ++answer;
+        }
+
     }
 
     return answer;
+}
+
+void main()
+{
+    solution("[](){}");
 }
