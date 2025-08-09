14d19c86400658ecddd5122a19a26f06b3e7237a
DEV_JSB <mvat1225@naver.com>
2025-08-09 20:32:34 +0900

    레벨3 표 편집 코드 개선

--- AlgorythmTest/AlgorythmTest/LV3_81303.cpp
+++ AlgorythmTest/AlgorythmTest/LV3_81303.cpp
@@ -2,38 +2,26 @@
 #include <vector>
 #include <stack>
 using namespace std;
-#include <string>
-#include <vector>
-#include <stack>
-using namespace std;
 
 
 string solution(int n, int k, vector<string> cmd)
 {
     string answer = "";
-    vector<int> graph;
-    stack<pair<int, int>> trashUpDown;
-    stack<int> trashIndex;
-    int nowIndex = k;
-    graph.resize(n, 1);
+    stack<int> deletedIndex;
 
     vector<int> up;
     vector<int> down;
 
-    up.resize(n + 2, 0);
-    down.resize(n + 2, 0);
-
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < n + 1; ++i)
     {
-        up[i + 1] = i - 1;
-        down[i + 1] = i + 1;
+        up.push_back(i - 1);
+        down.push_back(i + 1);
     }
 
-
+    ++k;
 
     for (int i = 0; i < cmd.size(); ++i)
     {
-        int nowIndexCorrect = nowIndex + 1;
         char command = cmd[i][0];
         switch (command)
         {
@@ -42,62 +30,43 @@ string solution(int n, int k, vector<string> cmd)
             int size = stoi(cmd[i].substr(2));
             for (int j = 0; j < size; ++j)
             {
-                nowIndex = up[nowIndexCorrect];
-                nowIndexCorrect = nowIndex + 1;
+                k = up[k];
             }
         }
-            break;
+        break;
         case 'D':
+        {
             int size = stoi(cmd[i].substr(2));
             for (int j = 0; j < size; ++j)
             {
-                nowIndex = down[nowIndexCorrect];
-                nowIndexCorrect = nowIndex + 1;
+                k = down[k];
             }
-            break;
+        }
+        break;
+
         case 'Z':
         {
-            graph[trashIndex.top()] = 1;
-            pair<int, int> trashedUpDown = trashUpDown.top();
-            down[trashedUpDown.first + 1] = trashIndex.top();
-            up[trashedUpDown.second + 1] = trashIndex.top();
-
-            trashUpDown.pop();
-            trashIndex.pop();
+            down[up[deletedIndex.top()]] = deletedIndex.top();
+            up[down[deletedIndex.top()]] = deletedIndex.top();
+            deletedIndex.pop();
             break;
         }
         case 'C':
-            graph[nowIndex] = 0;
-
-            trashIndex.push(nowIndex);
-            trashUpDown.push(make_pair(up[nowIndexCorrect], down[nowIndexCorrect]));
-
-            down[up[nowIndexCorrect] + 1] = down[nowIndexCorrect];
-            up[down[nowIndexCorrect] + 1] = up[nowIndexCorrect];
-            nowIndex = down[nowIndexCorrect] < n ? down[nowIndexCorrect] : up[nowIndexCorrect];
+            deletedIndex.push(k);
+            down[up[k]] = down[k];
+            up[down[k]] = up[k];
+            k = down[k] <= n ? down[k] : up[k];
             break;
         }
     }
 
-    for (int i = 0; i < graph.size(); ++i)
+    answer.append(n, 'O');
+
+    while (!deletedIndex.empty())
     {
-        answer.push_back(graph[i] > 0 ? 'O' : 'X');
+        answer[deletedIndex.top() - 1] = 'X';
+        deletedIndex.pop();
     }
 
     return answer;
-}
-
-void main()
-{
-    int n = 8;
-    int k = 2;
-    vector<string> cmd = { "D 2","C","U 3","C","D 4","C" , "U 2" , "Z" , "Z" , "U 1","C" };
-
-    string result = solution(n, k, cmd);
-}
-void main()
-{
-    solution(5, 3, { "U 3","C" }); // "OOOXO"
-
-
 }
