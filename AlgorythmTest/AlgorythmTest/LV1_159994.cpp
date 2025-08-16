0cdfd4ecad219c58d924201227c6aa3bf74749ed
DEV_JSB <mvat1225@naver.com>
2025-08-16 18:22:40 +0900

    레벨1 카드 뭉치 책 코드 추가
    책에서 나온 풀이 추가

--- AlgorythmTest/AlgorythmTest/LV1_159994.cpp
+++ AlgorythmTest/AlgorythmTest/LV1_159994.cpp
@@ -2,6 +2,43 @@
 #include <vector>
 #include <queue>
 using namespace std;
+string solution2(vector<string> cards1, vector<string> cards2, vector<string> goal)
+{
+    queue<string> c1,c2,g;
+
+    for (const string& str : cards1)
+    {
+        c1.push(str);
+    }
+    for (const string& str : cards2)
+    {
+        c2.push(str);
+    }
+    for (const string& str : goal)
+    {
+        g.push(str);
+    }
+
+    while (!g.empty())
+    {
+        if (!c1.empty() && c1.front() == g.front())
+        {
+            c1.pop();
+            g.pop();
+        }
+        else if (!c2.empty() && c2.front() == g.front())
+        {
+            c2.pop();
+            g.pop();
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return g.empty() ? "Yes" : "No";
+}
 
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) 
 {
