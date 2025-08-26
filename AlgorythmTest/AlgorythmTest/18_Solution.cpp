4f27430043e90edac16d6fcd48a9d2c72d21df28
DEV_JSB <mvat1225@naver.com>
2025-08-26 22:31:55 +0900

    책에서 나온 답안 코드

--- AlgorythmTest/AlgorythmTest/18_Solution.cpp
+++ AlgorythmTest/AlgorythmTest/18_Solution.cpp
@@ -4,6 +4,45 @@
 using namespace std;
 
 
+void mapping(vector<int>& hash, const vector<int>& arr, int target)
+{
+	for (int i = 0; i < arr.size(); ++i)
+	{
+		if (arr[i] > target)
+		{
+			continue;
+		}
+		hash[arr[i]] = 1;
+	}
+}
+
+bool bookSolution(vector<int> arr, int target)
+{
+	vector<int> hash(target + 1, 0);
+	mapping(hash, arr, target);
+
+	for (int i = 0; i < arr.size(); ++i)
+	{
+		int num = target - arr[i];
+
+		if (arr[i] == num)
+		{
+			continue;
+		}
+		if (num < 0)
+		{
+			continue;
+		}
+		if (hash[num])
+		{
+			return true;
+		}
+
+	}
+
+}
+
+
 bool solution(vector<int> arr, int target)
 {
 	unordered_map<int, int> map;
